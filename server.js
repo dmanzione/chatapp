6514f6ca5441cdcb3af178403153f24c39f66ecd
dmanzione <93665292+dmanzione@users.noreply.github.com>
2022-09-27 17:56:23 -0400

    changed styling of message components

---
+++ server.js
@@ -0,0 +1,37 @@
+
+//create application structure with lightweight web framework Express
+const app = require('express')();
+
+//get http module
+let http = require('http');
+
+//create server and associate said server with express app
+http = http.Server(app);
+
+//get socket.io module and tell it to put our socket at the http server's endpoint
+//took me a while to figure this out but all you have to do is extend the http
+//server to allow your server socket to listen for messages on it
+const io = require('socket.io')(http); 
+
+
+const port = process.env.PORT || 3000;
+
+
+//how express does routing
+app.get('/', (req, res) => {
+  //tell app that static files can be found in public folder
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+//socket.io handler for when a client makes a connection 
+io.on('connection', (socket) => {
+  
+  //you add handlers for messages and errors inside this one
+  socket.on('message', msg => {
+    io.emit('message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});

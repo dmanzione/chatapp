9f55eaddb0ffdacda4796d6056c7ac62e9830d94
dmanzione <93665292+dmanzione@users.noreply.github.com>
2022-09-28 02:05:36 -0400

    makes announcements to room when new connection is made

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ http = http.Server(app);
 //get socket.io module and tell it to put our socket at the http server's endpoint
 //took me a while to figure this out but all you have to do is extend the http
 //server to allow your server socket to listen for events on it
-const io = require('socket.io')(http); 
+const io = require('socket.io', { autoConnect: false })(http);
 
 
 const port = process.env.PORT || 3000;
@@ -19,21 +19,35 @@ const port = process.env.PORT || 3000;
 
 //how express does routing
 app.get('/', (req, res) => {
-  //tell app that static files can be found in public folder
-  res.sendFile(__dirname + '/public/index.html');
+	//tell app that static files can be found in public folder
+	res.sendFile(__dirname + '/public/index.html');
 });
 
 //socket.io handler for when a client makes a connection 
 io.on('connection', (socket) => {
-  
- 
- io.emit('broadcast',"client has joined the room");//this emits message to anyone connected 
-  //you add handlers for messages and errors inside this one
-  socket.on('message', msg => {
-    io.emit('message', msg);
-  });
+	
+
+	let chathandler = socket.handshake;
+//	if (sessionStorage.userFirstName == null) {
+//		chathandler = "client id " + socket.id;
+//	} else {
+//		chathandler = sessionStorage.userFirstName;
+//	}
+	let announcement = chathandler + " has entered the room";
+	
+	
+	//this sends the announcement to all connected clients except the new socket instance
+	socket.broadcast.emit('announcement',announcement);
+	
+	console.log("new connection: " + socket.id)//log to server console
+
+	
+	//you add handlers for messages and errors inside this one
+	socket.once('message', msg => {
+		io.emit('message', msg);
+	});
 });
 
 http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+	console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
